e2a4f9116ff67dcf7a6404ba7fe52315ae390d0c
Pascal Gattino <79808866+Pask42@users.noreply.github.com>
2025-08-06 19:04:47 +0200

    Add OrderReader currency test

---
+++ cii-messaging-parent/cii-reader/src/test/java/com/cii/messaging/reader/OrderReaderTest.java
@@ -0,0 +1,24 @@
+package com.cii.messaging.reader;
+
+import com.cii.messaging.model.CIIMessage;
+import com.cii.messaging.reader.impl.OrderReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderReaderTest {
+
+    @Test
+    void extractsCurrencyFromOrder() throws Exception {
+        OrderReader reader = new OrderReader();
+        URL resource = getClass().getResource("/order-sample.xml");
+        assertNotNull(resource);
+        File file = new File(resource.toURI());
+        CIIMessage message = reader.read(file);
+        assertNotNull(message.getHeader());
+        assertEquals("EUR", message.getHeader().getCurrency());
+    }
+}
